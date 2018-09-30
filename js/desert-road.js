e332d63bd6b6731c0b4da3dc02cfacf146f992ad
flucas <flucas@visiativ.com>
2018-09-30 12:58:30 +0200

    invert speed buttons

--- js/desert-road.js
+++ js/desert-road.js
@@ -14,8 +14,8 @@ $(function() {
 function changeSpeed(speedup){
     let $roadLineSelector = $('.road .line');
     let animDuration = Number($roadLineSelector.css('animation-duration').replace("s",""));
-    animDuration = speedup ? animDuration + 0.1 : animDuration - 0.1;
-    if(animDuration < 0){
+    animDuration = speedup ? animDuration - 0.2 : animDuration + 0.2;
+    if(animDuration <= 0){
         animDuration = 0.2;
     }
     $roadLineSelector.css('animation-duration', animDuration + "s");
